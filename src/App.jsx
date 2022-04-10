1c658f5e0478e6a6728d71d796ccff53ba67351a
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-04-10 09:41:41 +0800

    Initial Commit - Added Components "game.jsx"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Game from "./components/game";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Game />
+    </>
+  );
+};
+
+export default App;
